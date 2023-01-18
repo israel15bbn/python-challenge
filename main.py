dfa2e6ace8192be7d1990af61a909ca85ba0d598
Israel Martinez <120421590+israel15bbn@users.noreply.github.com>
2023-01-17 23:43:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import csv
 import os
 
-file = os.path.join("election_data.csv")
+file = os.path.join("budget_data.csv")
 
 with open(file) as csvfile:
     csvreader = csv.reader(csvfile)
     header = next(csvreader)
 
-    candidates = {}
-
-    for row in csvreader:
-        candidate = row[2]
-        if candidate in candidates:
-            candidates[candidate] += 1
-        else:
-            candidates[candidate] = 1
-
-    for key, value in candidates.items():
-        print(key, value)
-        
+    prev_rev = 0
+    rev_change_list = []
+ 
+    rev_change = float(row[1]) - (prev_rev)
+    prev_rev = float(row[1])
+    rev_change_list = rev_change_list + [rev_change]
+ 
+    rev_change_list.pop(0)    
+    rev_avg = sum(rev_change_list)/len(rev_change_list)
